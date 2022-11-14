1ac3118fa5dc38f20f864422915bc8bb1df41613
Chloe Decker <69217587+TheOnlyMrLucifer@users.noreply.github.com>
2022-11-14 16:05:16 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ echo "
 ███████╗██╔╝╚██╗░░░██║░░░██║░░██║███████╗██║░╚═╝░██║██║██████╔╝
 ╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝╚═╝╚═════╝░
 hey master
-DevilUserBot in your service.........
+BarbieGroupManagementBot in your service.........
 "
 
 python -m userbot
